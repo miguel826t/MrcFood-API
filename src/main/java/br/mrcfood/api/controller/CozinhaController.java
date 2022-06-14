8a14c5b49a86a24b88b5f5807421014fde89d367
miguel826t <miguel826t@gmail.com>
2022-06-14 15:55:07 -0300

    4.13-Implementando content negotiation para retornar Json ou Xml

--- src/main/java/br/mrcfood/api/controller/CozinhaController.java
+++ src/main/java/br/mrcfood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package br.mrcfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,8 @@ public class CozinhaController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	//Define o tipo que este metodo retorna (produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
 		return cozinhaRepository.buscarAll();
 	}
