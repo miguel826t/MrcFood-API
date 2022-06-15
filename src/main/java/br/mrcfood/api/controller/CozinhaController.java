1399b983a94cab2131925ca7e1695c9dd47f4a8a
miguel826t <miguel826t@gmail.com>
2022-06-15 11:17:19 -0300

    4.20-Manipulando a resposta HTTP com ResponseEntity

--- src/main/java/br/mrcfood/api/controller/CozinhaController.java
+++ src/main/java/br/mrcfood/api/controller/CozinhaController.java
@@ -3,11 +3,15 @@ package br.mrcfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.ServerRequest.Headers;
 
 import br.mrcfood.api.model.CozinhasXml;
 import br.mrcfood.domain.entity.Cozinha;
@@ -33,8 +37,21 @@ public class CozinhaController {
 	}
 	
 	@GetMapping(value = "/{cozinhaId}",produces = MediaType.APPLICATION_XML_VALUE)
-	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
-		return cozinhaRepository.buscarPorId(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.buscarPorId(id);
+		
+		/* ======* Formas de retornar Status *====== */
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		//return ResponseEntity.ok(cozinha);  //Retonar o OK com conteudo
+		//return ResponseEntity.ok().build(); //Retornaria OK mas sem conteudo
+		
+		/* ======* Retornar um redirected com um Header com a new location *====== */
+		HttpHeaders headers = new HttpHeaders(); //org.springframework.http.HttpHeaders
+		headers.add(HttpHeaders.LOCATION,"http://localhost:8080/cozinhas");
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 }
